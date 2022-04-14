9a6535d6bd694ef54914df3f61cda504aca64b08
Dima Shevtsov <shevadima15z@gmail.com>
2022-04-14 15:09:28 +0300

    Added InputService, HeroMovement, CameraFollowing

--- src/KnowledgeIsPower/Assets/CodeBase/Infrastructure/Game.cs
+++ src/KnowledgeIsPower/Assets/CodeBase/Infrastructure/Game.cs
@@ -1,7 +1,25 @@
-﻿namespace CodeBase.Infrastructure
+﻿using CodeBase.Services.Input;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure
 {
     public class  Game
     {
-        
+        public static IInputService InputService;
+
+        public Game()
+        {
+            RegisterInputService();
+        }
+
+        private static void RegisterInputService()
+        {
+            if (Application.isEditor)
+                InputService = new StandaloneInputService();
+            else
+            {
+                InputService = new MobileinputService();
+            }
+        }
     }
 }
---
+++ src/KnowledgeIsPower/Assets/CodeBase/Services/Input/MobileinputService.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Services.Input
+{
+    public class MobileinputService : InputService
+    {
+        public override Vector2 Axis => SimpleInputAxis();
+    }
+}
